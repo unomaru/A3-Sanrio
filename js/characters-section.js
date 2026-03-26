f246c45c5589033d5c2a1ada940816ebd0c47cf9
Chihana Kashiwabara <c.kashiwabara@mail.utoronto.ca>
2026-03-26 11:58:08 -0400

    character specific graph

--- js/characters-section.js
+++ js/characters-section.js
@@ -2,6 +2,18 @@
     const grid = d3.select("#character-grid");
     const tooltip = d3.select("#tooltip");
 
+    const detailModal = d3.select("#character-detail-modal");
+    const detailTitle = d3.select("#detail-title");
+    const detailSvg = d3.select("#detail-chart");
+    const detailCloseBtn = d3.select("#detail-close-btn");
+
+    const detailWidth = +detailSvg.attr("width");
+    const detailHeight = +detailSvg.attr("height");
+
+    const detailMargin = { top: 30, right: 30, bottom: 50, left: 60 };
+    const detailInnerWidth = detailWidth - detailMargin.left - detailMargin.right;
+    const detailInnerHeight = detailHeight - detailMargin.top - detailMargin.bottom;
+
     const imageMap = {
         "Hello Kitty": "images/kitty.webp",
         "Little Twin Stars": "images/little twin stars.gif",
@@ -102,7 +114,13 @@
             })
             .on("mouseout", function() {
                 tooltip.classed("hidden", true);
-            });
+            })
+            .on("click", function(event, d) {
+                cards.classed("selected", false);
+                d3.select(this).classed("selected", true);
+                tooltip.classed("hidden", true);
+                drawDetailChart(d.character, data);
+            });;
 
         cards.append("img")
             .attr("src", d => d.image)
@@ -111,5 +129,78 @@
         cards.append("div")
             .attr("class", "character-name")
             .text(d => d.character);
+
+        function drawDetailChart(character, allData) {
+            detailModal.classed("hidden", false);
+            detailTitle.text(`${character} Ranking History (2000–2024)`);
+
+            const years = d3.range(2000, 2025);
+
+            const characterRows = allData.filter(d => d.character === character);
+            const rankByYear = new Map(characterRows.map(d => [d.year, d.rank]));
+
+            const series = years.map(year => ({
+                year,
+                rank: rankByYear.has(year) ? rankByYear.get(year) : null
+            }));
+
+            detailSvg.selectAll("*").remove();
+
+            const g = detailSvg.append("g")
+                .attr("transform", `translate(${detailMargin.left},${detailMargin.top})`);
+
+            const x = d3.scaleLinear()
+                .domain([2000, 2024])
+                .range([0, detailInnerWidth]);
+
+            const y = d3.scaleLinear()
+                .domain([10, 1])
+                .range([detailInnerHeight, 0]);
+
+            const xAxis = d3.axisBottom(x)
+                .tickValues([2000, 2005, 2010, 2015, 2020, 2024])
+                .tickFormat(d3.format("d"));
+
+            const yAxis = d3.axisLeft(y)
+                .tickValues(d3.range(1, 11))
+                .tickFormat(d => `#${d}`);
+
+            g.append("g")
+                .attr("transform", `translate(0,${detailInnerHeight})`)
+                .call(xAxis);
+
+            g.append("g")
+                .call(yAxis);
+
+            const line = d3.line()
+                .defined(d => d.rank !== null)
+                .x(d => x(d.year))
+                .y(d => y(d.rank));
+
+            g.append("path")
+                .datum(series)
+                .attr("fill", "none")
+                .attr("stroke", "#e7549d")
+                .attr("stroke-width", 3)
+                .attr("d", line);
+
+            g.selectAll(".point")
+                .data(series.filter(d => d.rank !== null))
+                .join("circle")
+                .attr("cx", d => x(d.year))
+                .attr("cy", d => y(d.rank))
+                .attr("r", 4)
+                .attr("fill", "#e7549d");
+        }
+
+        detailCloseBtn.on("click", function () {
+            detailModal.classed("hidden", true);
+        });
+
+        detailModal.on("click", function (event) {
+            if (event.target === this) {
+                detailModal.classed("hidden", true);
+            }
+        });
     });
 })();
